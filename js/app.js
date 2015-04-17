9b1d172fdaa755495dea60b479d9283ee504ddbe
jdalton308 <jdalton308@yahoo.com>
2015-04-17 16:32:31 -0700

    began adding Firebase integration for user and backend support

--- js/app.js
+++ js/app.js
@@ -1,5 +1,86 @@
-angular.module('spotBeta', []);
+var app = angular
+	.module('spotBeta', [
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'firebase'
+	])
+	.constant('FIREBASE_URL', 'https://intense-inferno-4354.firebaseio.com' )
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'index.html',
+				controller: 'mainController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
 
-spotBeta.controller('mainController', ['$scope', function($scope){
+app.controller('mainController', ['$scope', '$location', 'Auth', function($scope, $location, Auth){
 
+	// USER AUTHENTICATION
+	//TODO: change the paths here from '/' to the dashboard template
+
+	if (Auth.signedIn) {
+		$location.path('/');
+	}
+
+	$scope.login = function() {
+		Auth.login($scope.user).then(function(){
+			$location.path('/');
+		});
+	}
+
+	$scope.register = function() {
+		Auth.register($scope.user).then(function(){
+			return Auth.login($scope.user).then(function(){
+				$location.path('/');
+			});
+		});
+	}
+
+	$scope.signedIn = Auth.signedIn;
+	$scope.logout = Auth.logout;
 }]);
+
+app.factory('callFire', function($firebase, FIREBASE_URL){
+	var ref = new Firebase(FIREBASE_URL);
+
+});
+
+app.factory('Auth', function($firebaseSimpleLogin, FIREBASE_URL, $rootScope) {
+	var ref = new Firebase(FIREBASE_URL);
+	var auth = $firebaseSimpleLogin(ref);
+
+	var Auth = {
+		register: function(user) {
+			return auth.$createUser(user.email, user.password);
+		},
+		login: function(user) {
+			return auth.$login('password', user);
+		},
+		logout: function() {
+			auth.$logout();
+		},
+		resolveUser: function() {
+			return auth.$getCurrentUser();
+		},
+		signedIn: function() {
+			return !!Auth.user.provider;
+		},
+		user: {}
+	};
+
+	$rootScope.$on('$firebaseSimpleLogin:login', function(e, user) {
+		console.log('logged in');
+		angular.copy(user, Auth.user);
+	});
+	$rootScope.$on('$firebaseSimpleLogin:logout', function(){
+		console.log('logged out');
+		angular.copy({}, Auth.user);
+	})
+})
