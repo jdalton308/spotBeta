a66db02c74112086b398e2da0045a2f8dd4e89ec
Joe Dalton <joseph.dalton@level-studios.com>
2015-06-04 17:40:52 -0700

    Finished upload capability when using an existing climb spot and a boulder. Need to add logic and data structure for routes and creating a new spot

--- js/directive.mapselect.js
+++ js/directive.mapselect.js
@@ -1,10 +1,6 @@
 app.directive('jdMapSelect', [ function(){
 	return {
 		restrict: 'E',
-		// require: '^jdUploadView',
-		// scope: {
-		// 	newClimb: '=newclimb'
-		// },
 		templateUrl: 'directives/mapSelect.html',
 		link: function(scope, element, attributes) {
 
@@ -27,7 +23,7 @@ app.directive('jdMapSelect', [ function(){
 			var mapElement = document.getElementById("locationMapCanvas");
 			var mapOptions = {
 				center: mapCenter,
-				zoom: 10,
+				zoom: 5,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 
@@ -42,38 +38,51 @@ app.directive('jdMapSelect', [ function(){
 				draggable: true,
 			});
 
-			console.log('New Climb Obj:');
-			console.log(scope.newClimb);
-
-			console.log('Scope:');
-			console.log(scope);
-
 
 			// EVENTS
 			//=====================
+			// Fix map rendering bug
+			scope.resizeMap = function(){
+				google.maps.event.trigger(scope.locationMap, 'resize');
+				scope.locationMap.setZoom( scope.locationMap.getZoom() )
+				scope.locationMap.setCenter( scope.locationMarker.getPosition() );
+			};
 
-			// Add keyup events to lat,lng inputs for when updating manually. => Pan the map and marker
+			// Typing Lat, Lng
+			//---------------------
+			scope.manualCoordUpdate = function(){
+				var center = new google.maps.LatLng(scope.newClimb.newLocation.latitude, scope.newClimb.newLocation.longitude);
+				scope.locationMap.setCenter(center);
+				scope.locationMarker.setPosition(center);
+			};
 
 			// Marker Drag
+			//-----------------
 			google.maps.event.addListener(scope.locationMarker, 'drag', function(){
 				// update the scope's newLat and newLong
 				var markerPos = scope.locationMarker.getPosition();
 
-				console.log('MarkerPos:');
-				console.log(markerPos);
-
 				scope.$apply( scope.newClimb.newLocation.latitude = markerPos.lat() );
 				scope.$apply( scope.newClimb.newLocation.longitude = markerPos.lng() );
-
-				console.log('New Marker Lat: '+ scope.newClimb.newLocation.latitude);
-				// console.log('New Marker Lat: '+ markerPos.lat());
-				console.log('New Marker Lng: '+ scope.newClimb.newLocation.longitude);
-				// console.log('New Marker Lng: '+ markerPos.lng());
 			});
 
+			// Center Marker
+			//-----------------
+			scope.centerMarker = function() {
+				// Get map center
+				var center = scope.locationMap.getCenter();
+				// Set marker position
+				scope.locationMarker.setPosition(center);
+			};
+
 			// Marker Lock and Map Drag
+			//------------------------------
 			scope.markerLocked = false;
+			var dragStart;
+			var dragMove;
+
 			scope.toggleMarkerLock = function() {
+
 				scope.markerLocked = !scope.markerLocked;
 
 				if (scope.markerLocked){
@@ -84,7 +93,7 @@ app.directive('jdMapSelect', [ function(){
 					var markerStartLat;
 					var markerStartLng;
 
-					var dragStart = google.maps.event.addListener(scope.locationMap, 'dragstart', function(){
+					dragStart = google.maps.event.addListener(scope.locationMap, 'dragstart', function(){
 						var startMapCenter = scope.locationMap.getCenter();
 						mapStartLat = startMapCenter.lat();
 						mapStartLng = startMapCenter.lng();
@@ -93,7 +102,7 @@ app.directive('jdMapSelect', [ function(){
 						markerStartLat = markerPos.lat();
 						markerStartLng = markerPos.lng();
 					});
-					var dragMove = google.maps.event.addListener(scope.locationMap, 'drag', function(){
+					dragMove = google.maps.event.addListener(scope.locationMap, 'drag', function(){
 						// Move the marker the same distance as the map
 						var newMapCenter = scope.locationMap.getCenter();
 						mapNewLat = newMapCenter.lat();
@@ -102,15 +111,12 @@ app.directive('jdMapSelect', [ function(){
 						var movedLat = mapStartLat - mapNewLat;
 						var movedLng = mapStartLng - mapNewLng;
 
-						var newMarkerLat = markerStartLat + movedLat;
-						var newMarkerLng = markerStartLng + movedLng;
+						var newMarkerLat = markerStartLat - movedLat;
+						var newMarkerLng = markerStartLng - movedLng;
 
 						// update scope
-						scope.newClimb.location.newLat = newMarkerLat;
-						scope.newClimb.location.newLng = newMarkerLng;
-
-						console.log('New Marker Lat: '+ newMarkerLat);
-						console.log('New Marker Lng: '+ newMarkerLng);
+						scope.$apply( scope.newClimb.newLocation.latitude = newMarkerLat );
+						scope.$apply( scope.newClimb.newLocation.longitude = newMarkerLng );
 
 						// update position
 						var updatedMarkerPos = new google.maps.LatLng(newMarkerLat, newMarkerLng);
--- js/directive.uploadview.js
+++ js/directive.uploadview.js
@@ -2,24 +2,6 @@ app.directive('jdUploadView', ['ClimbData', function(ClimbData){
 	return {
 		restrict: 'E',
 		templateUrl: 'directives/uploadView.html',
-		// controller: function($scope){
-
-		// 	// Attach climb data to scope
-		// 	//--------------------------------
-		// 	ClimbData.$loaded(function(data){
-		// 		$scope.climbData = data;
-		// 		console.log('ClimbData in Upload View');
-		// 		console.log($scope.climbData);
-		// 	});
-
-		// 	// Form Data
-		// 	//-------------------------------
-		// 	$scope.newClimb = {};
-
-		// 	// These are just for reference
-		// 	$scope.newClimb['newLocation'] = {};
-
-		// },
 		link: function(scope, element, attributes) {
 
 			// Attach climb data to scope
@@ -40,8 +22,49 @@ app.directive('jdUploadView', ['ClimbData', function(ClimbData){
 			// Submit
 			//--------------------------
 			scope.createNewClimb = function() {
+
+				// TODO: Process the grade
+				// // if boulder, unsure only number
+				// // if roped, create grade conversion and translated grade
+
 				console.log('Upload Form:');
 				console.log(scope.uploadForm);
+
+				// console.log('New Climb:');
+				// console.log(scope.newClimb);
+
+				if (scope.newClimb.newOrOld == 'old') {
+					var targetSpot = scope.climbData[scope.newClimb.climbLocation];
+					// console.log('Target Spot:');
+					// console.log(targetSpot);
+
+					var climbObj = {
+						description: scope.newClimb.climbDescription,
+						grade: parseInt(scope.newClimb.climbGrade), //if type == boulder
+						height: scope.newClimb.climbHeight,
+						name: scope.newClimb.climbName,
+						rating: parseInt(scope.newClimb.climbRating),
+						type: scope.newClimb.climbType
+					};
+
+					console.log('New Climb Obj:');
+					console.log(climbObj);
+
+					targetSpot.climbs.push(climbObj);
+					console.log('New Target Spot:');
+					console.log(targetSpot);
+
+					scope.climbData.$save().then(
+						function(data){
+							console.log('Climb successfully added to database');
+							console.log('New Data:')
+							console.log(data);
+						},
+						function(err){
+							console.error('Error adding new climb to database. '+ err);
+						}
+					);
+				}
 			};
 
 			scope.showPreview = true;
